fcc241c33772dd773e1aaac9900785555c54735f
Aswathy-mp <aswathymp24@gmail.com>
2022-04-11 14:51:03 +0530

    commit

--- src/routes/addauthorRoutes.js
+++ src/routes/addauthorRoutes.js
@@ -17,7 +17,7 @@ addauthorRouter.use(multer({storage:imageStore}).single('image'));
 
 function router(nav){
     addauthorRouter.get('/',function(req,res){
-        res.render('addAuthor',{
+        res.render('addauthor',{
             nav
         })
     })
